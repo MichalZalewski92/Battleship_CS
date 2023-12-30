031cb1665b22112ce871bfcbacabe3e5dd29d5c1
MichalZalewski92 <michal.soski@gmail.com>
2023-12-30 17:32:06 +0100

    header logo + ship creator

--- Battleship/Display.cs
+++ Battleship/Display.cs
@@ -21,6 +21,20 @@ namespace Battleship
         {
             Console.WriteLine("Invalid input. Please enter a valid option.");
         }
+        public static void ShowFlashScreen() //logo startowe gry 
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("        ********************************************");
+            Console.Write("        *"); Console.ForegroundColor = ConsoleColor.White; Console.Write("             Battleship Game              "); Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*");
+            Console.WriteLine("        ********************************************");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Timer t = new Timer(ClearFlashScreen, null, 0, 1000);
+            Thread.Sleep(2100);
+            t.Dispose();
+
+        }
 
         // dodać pozostałe metody dispalya
     }
